10707953b3cdd99e567c11f63e3a7005e737fab0
Manish <manish.gupta@v2solutions.com>
2018-01-08 17:41:56 +0530

    commit2

--- func1.js
+++ func1.js
@@ -1,30 +1,23 @@
 // Functions
 
-
-function add(x , y){
-    return x+y;
-}
-
-
-function mul(x , y){
-    return x*y;
-}
-
-function operation(func){
-return func;
-}
-
-
-/*
-function operation(func, a)(b){
-    function func(a) {
-        console.log(a + b);
+function add(x){
+    var a = x;
+    function add1(b){
+        return a+b;
     }
+}
 
-    function func(a) {
-        console.log(a * b);
+function mul(x){
+    var a = x;
+    function mul1(b){
+        return a*b;
     }
-};*/
+}
 
-console.log(operation((add)(10 , 20)));
-console.log(operation((mul) (10, 20)));
+function operation(func, x){
+return func(x);
+}
+
+
+console.log(operation((add,10)(20)));
+console.log(operation((mul,10)(20)));
